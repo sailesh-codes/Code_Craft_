443320398078e8839f68dcf811d0eeedc2e57948
Sailesh T <saileshtrn06@gmail.com>
2025-12-15 20:52:28 +0530

    fix: update Vercel and Vite config for production build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : '/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
